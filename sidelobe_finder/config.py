a7357eee68b0cc595ccae9c95ad30820fa2687ec
riggi <simone.riggi@gmail.com>
2020-01-27 17:42:33 +0100

    Add additional anchor box sizes

--- sidelobe_finder/config.py
+++ sidelobe_finder/config.py
@@ -20,7 +20,8 @@ class Config:
 		self.anchor_box_scales = [4, 8, 16, 32]
 
 		# anchor box ratios
-		self.anchor_box_ratios = [[1, 1], [1./math.sqrt(2), 2./math.sqrt(2)], [2./math.sqrt(2), 1./math.sqrt(2)]]
+		#self.anchor_box_ratios = [[1, 1], [1./math.sqrt(2), 2./math.sqrt(2)], [2./math.sqrt(2), 1./math.sqrt(2)]]
+		self.anchor_box_ratios = [ [1,1], [1,2], [2,1], [1,3], [3,1] ]
 
 		# size to resize the smallest side of the image
 		#self.im_size = 600
